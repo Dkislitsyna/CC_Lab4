0868d32df3446c84bca6bee910ebbc0589b4b797
di <doshiiiik@icloud.com>
2023-06-05 00:08:09 +0700

    Create an iStorage interface

--- Last/Models/Car.cs
+++ Last/Models/Car.cs
@@ -1,13 +1,36 @@
 ﻿using System;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
 
 namespace Last.Models
 {
     public class Car
     {
+            public Guid Id { get; set; } = Guid.Empty;
             public string Brand { get; set; }
 
             public string Model { get; set; }
 
             public int Price { get; set; }
+
+            public BaseModelValidationResult Validate()
+            {
+                var validationResult = new BaseModelValidationResult();
+
+                if (string.IsNullOrWhiteSpace(Brand)) validationResult.Append($"Name cannot be empty");
+                if (string.IsNullOrWhiteSpace(Model)) validationResult.Append($"Surname cannot be empty");
+                if (!(0 < Price)) validationResult.Append($"Price {Price} is out of range 0...");
+
+                if (!string.IsNullOrEmpty(Brand) && !char.IsUpper(Brand.FirstOrDefault())) validationResult.Append($"Name {Brand} should start from capital letter");
+                if (!string.IsNullOrEmpty(Model) && !char.IsUpper(Model.FirstOrDefault())) validationResult.Append($"Surname {Model} should start from capital letter");
+
+                return validationResult;
+            }
+
+            public override string ToString()
+            {
+                return $"{Brand} {Model} cost {Price} rubles";
+            }
     }
 }
---
+++ Last/Storage/Exceptions.cs
@@ -0,0 +1,16 @@
+﻿namespace Last.Storage
+{
+    public class Exceptions
+    {
+        [System.Serializable]
+        public class IncorrectAvtoException : System.Exception
+        {
+            public IncorrectAvtoException() { }
+            public IncorrectAvtoException(string message) : base(message) { }
+            public IncorrectAvtoException(string message, System.Exception inner) : base(message, inner) { }
+            protected IncorrectAvtoException(
+                System.Runtime.Serialization.SerializationInfo info,
+                System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+        }
+    }
+}
